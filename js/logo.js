9978f056c6886586206b3a1bac42af8a284a78b7
jandrwiega <60856871+jandrwiega@users.noreply.github.com>
2020-05-25 00:03:11 +0200

    basic platform

--- js/logo.js
+++ js/logo.js
@@ -5,22 +5,22 @@ const footer = document.querySelector(".footer__box");
 const logo = document.querySelector(".logo__box--photo");
 const hamburgerbox = document.querySelector(".logo__hamburger");
 const allbox = document.querySelector(".wrapper");
+const optionbox = document.querySelector(".logo__box--hamburger");
 let topposition = hamburgerbox.offsetTop;
 let screenwindow = window.innerWidth;
-console.log(screenwindow);
-
+let photoposition = ((screenwindow - 200)/2)+"px";
+logo.style.paddingLeft=photoposition;
 function bodyresize()
 {
     screenwindow = window.innerWidth;
-    console.log(screenwindow);
+     photoposition = ((screenwindow - 200)/2) + "px";
+     logo.style.paddingLeft=photoposition;
 }
-
 const change = () =>
 {
     hamburger.classList.toggle("hamburger--active");
     nav.classList.toggle("navigation--active");
     hamburgerbox.style.position="relative";
-    console.log(topposition);
     topposition = hamburgerbox.offsetTop;
    if(topposition == 95 || topposition == 60)
     {
@@ -30,22 +30,27 @@ const change = () =>
         }
         else
         {
-            hamburgerbox.style.top="-60px";
+            hamburgerbox.style.position="relative";
+           hamburgerbox.style.top="-60px";
+            
         }
     }
     else{
         hamburgerbox.style.top="0px";
     }
+    
 }
 hamburger.addEventListener("click", change);
-
 const gback = () =>
 {
     hamburger.classList.remove("hamburger--active");
     nav.classList.remove("navigation--active");
+    topposition = hamburgerbox.offsetTop;
+    if(screenwindow > 800)
+    {
+    hamburgerbox.style.top="0px";
+    }
 }
 all.addEventListener("click", gback);
-//hamburger.addEventListener("click", gback)
-//addedfooter.addEventListener("click",gback);
 footer.addEventListener("click", gback);
 logo.addEventListener("click", gback);
